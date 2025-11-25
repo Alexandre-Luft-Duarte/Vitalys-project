cb1285dcd5f1d1d2ffcc0658d8ba6ecdd16cf882
Alexandre-Luft-Duarte <alexandreduarte43234@gmail.com>
2025-11-24 21:38:41 -0300

    upload

--- banco/politica-acesso.sql
+++ banco/politica-acesso.sql
@@ -1,20 +1,15 @@
--- Criar Roles
 CREATE ROLE role_admin;
 CREATE ROLE role_medico;
 CREATE ROLE role_atendimento;
 
--- Admin: Acesso total
 GRANT ALL PRIVILEGES ON DATABASE hospital_db TO role_admin;
 
--- Médicos: Podem ver pacientes e gerenciar atendimentos/internações
 GRANT SELECT ON paciente, pessoa, vw_relatorio_atendimentos, vw_mapa_internacao TO role_medico;
 GRANT INSERT, UPDATE ON atendimento, anotacao_medica, internacao, evolucao_internacao TO role_medico;
 
--- Recepção: Cadastra pessoas e cria atendimentos
 GRANT INSERT, UPDATE, SELECT ON pessoa, paciente, endereco, contato TO role_atendimento;
 GRANT INSERT ON atendimento TO role_atendimento;
 
--- Criar usuários de exemplo
 CREATE USER usr_admin WITH PASSWORD 'admin123';
 GRANT role_admin TO usr_admin;
 
